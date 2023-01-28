12276eaf6312bb21907d9a9013cb850d623ce3d2
Rehmet Yeshanew <ryfgmz87@gmail.com>
2023-01-27 23:41:26 -0500

    tipping function

--- ratingcontract/rating.py
+++ ratingcontract/rating.py
@@ -44,4 +44,53 @@ class Rating(Application):
             App.globalPut(self.Variables.rating, Int(0)),
             Approve()
         ])
+    
+    def tip(self, amount: abi.Uint64):
+        count = Txn.application_args[1]
+        valid_number_of_transactions = Global.group_size() == Int(2)
+
+        valid_payment_to_seller = And(
+            Gtxn[1].type_enum() == TxnType.Payment,
+            Gtxn[1].receiver() == Global.creator_address(),
+            Gtxn[1].amount() == amount.get(),
+            Gtxn[1].sender() == Gtxn[0].sender(),
+        )
+
+        can_tip = And(valid_number_of_transactions,
+                      valid_payment_to_seller)
+
+        state_value = 'Successfully tipped'
+
+        return If(can_tip).Then(state_value).Else(Reject())
+    
+    # def rate(self):
+    #     count = Txn.application_args[1]
+    #     valid_number_of_transactions = Global.group_size() == Int(2)
+
+    #     valid_payment_to_seller = And(
+    #         Gtxn[1].type_enum() == TxnType.Payment,
+    #         Gtxn[1].receiver() == Global.creator_address(),
+    #         Gtxn[1].amount() == App.globalGet(self.Variables.price) * Btoi(count),
+    #         Gtxn[1].sender() == Gtxn[0].sender(),
+    #     )
+
+    #     can_buy = And(valid_number_of_transactions,
+    #                   valid_payment_to_seller)
+
+    #     update_state = Seq([
+    #         App.globalPut(self.Variables.sold, App.globalGet(self.Variables.sold) + Btoi(count)),
+    #         Approve()
+    #     ])
+
+    #     return If(can_buy).Then(update_state).Else(Reject())
+
+    def application_deletion(self):
+        return Return(Txn.sender() == Global.creator_address())
+
+    def application_start(self):
+        return Cond(
+            [Txn.application_id() == Int(0), self.application_creation()],
+            [Txn.on_completion() == OnComplete.DeleteApplication, self.application_deletion()],
+            [Txn.application_args[0] == self.AppMethods.buy, self.buy()]
+        )
 
